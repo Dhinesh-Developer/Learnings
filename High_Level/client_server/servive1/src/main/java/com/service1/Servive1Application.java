f0fcf888abfc94327e408e1fe01ed6e97118c56f
Dhinesh-Developer <dhineshdeveloper07@gmail.com>
2026-05-24 20:01:00 +0530

    Today high Level Design Module 1 : Client Server Architecture Praticedgit add .

---
+++ High_Level/client_server/servive1/src/main/java/com/service1/Servive1Application.java
@@ -0,0 +1,13 @@
+package com.service1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Servive1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Servive1Application.class, args);
+	}
+
+}
---
+++ High_Level/client_server/servive1/src/main/java/com/service1/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.service1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.service1.entity.User;
+
+
+public interface UserRepository extends JpaRepository<User,Long>{
+
+    
+} 
